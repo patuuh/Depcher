cb9f357c48ed209d883d47cd3a280e8e57ce0bf9
patuuh <44578687+patuuh@users.noreply.github.com>
2021-06-18 13:08:49 +0300

    Update run.py

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ from Wappalyzer import Wappalyzer, WebPage
 '''
 
 if os.path.isfile('bounty-targets-data'):
-    os.system('git clone https://github.com/arkadiyt/bounty-targets-data.git')
+	os.system('git clone https://github.com/arkadiyt/bounty-targets-data.git')
 else:
-    os.system('cd bounty-targets-data && git pull')
+	os.system('cd bounty-targets-data && git pull')
 
 wappalyzer = Wappalyzer.latest()
 
@@ -20,31 +20,33 @@ targets = json.load(targets_json)
 file_to_save = open("versions.txt","w+")
 
 for target in targets:
-    name = target['name']
-    file_to_save.write(name + "\n---------------\n")
-    for target_url in target['targets']['in_scope']:
-        asset_type = target_url['asset_type']
-        if 'URL' not in asset_type:
-            continue
-        url = target_url['asset_identifier']
-        if '*' in url or ',' in url: # TODO CHANGE THIS ',' to actually split multiple target urls
-             continue
-        if 'http' not in url:
-             url = "https://" + url
-             
-        print("Analyzing " + url + " ...")
-        try:
-            webpage = WebPage.new_from_url(url)
-            results = wappalyzer.analyze_with_versions(webpage)
-        except:
-            print("Following url failed for some reason: " + url)
-        file_to_save.write("--" + url + "\n")
-        for result in results:
-            version = results[result]['versions']
-            version_parsed = version.replace("[]", "")
-            app = result
-            if '[]' in str(version):
-                 continue
-            file_to_save.write("----" + app + ": " + str(version_parsed) + "\n")
-    file_to_save.write("###################\n\n")
-    
+	name = target['name']
+	file_to_save.write(name + "\n---------------\n")
+	for target_url in target['targets']['in_scope']:
+		asset_type = target_url['asset_type']
+		if 'URL' not in asset_type:
+			continue
+		url = target_url['asset_identifier']
+		if '*' in url or ',' in url: # TODO CHANGE THIS ',' to actually split multiple target urls
+			 continue
+		if 'http' not in url:
+			 url = "https://" + url
+			 
+		print("Analyzing " + url + " ...")
+		try:
+			webpage = WebPage.new_from_url(url)
+			results = wappalyzer.analyze_with_versions(webpage)
+		except:
+			print("Following url failed for some reason: " + url)
+		file_to_save.write("--" + url + "\n")
+		for result in results:
+			version = results[result]['versions']
+			version_parsed = version.replace("[]", "")
+			app = result
+			if '[]' in str(version):
+				 continue
+			file_to_save.write("----" + app + ": " + str(version_parsed) + "\n")
+	file_to_save.write("###################\n\n")
+			
+
+
